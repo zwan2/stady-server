66912957f370bafacd80e65745f4b01db09c1ef9
zwan2 <2zwan@naver.com>
2018-09-17 16:28:09 +0900

    0.2.42
    Address

--- routes/stopwatch.js
+++ routes/stopwatch.js
@@ -16,7 +16,7 @@ router.get('/', function (req, res, next) {
 // REQ: userId, examaddress, subjectId, studyId, startPoint, endPoint, term
 router.post('/stop', function(req, res, next) {
     var queryInsertData = "INSERT INTO histories (user_id, exam_address, subject_id, study_id, start_point, end_point, term) VALUES (?, ?, ?, ?, ?, ?, ?);";
-     db.get().query(queryInsertData, [req.body.userId, req.body.examaddress, req.body.subjectId, req.body.studyId, req.body.startPoint, req.body.endPoint, req.body.term], function (err, rows) {
+     db.get().query(queryInsertData, [req.body.userId, req.body.examAddress, req.body.subjectId, req.body.studyId, req.body.startPoint, req.body.endPoint, req.body.term], function (err, rows) {
         if (err) {
             return res.status(400).send(err);
         } else {
