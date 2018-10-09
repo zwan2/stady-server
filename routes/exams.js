004393cd4ee78915abdbef0e1b0351546ba132f7
zwan2 <2zwan@naver.com>
2018-10-09 15:09:31 +0900

    exam subject id send loadDatData

--- routes/exams.js
+++ routes/exams.js
@@ -12,19 +12,21 @@ router.get('/', function (req, res, next) {
 //REQ: req.query.examCat, req.query.examParentId
 router.get('/selectExam', function (req, res, next) {
     if (req.query.examCat == 0 && req.query.examParentId == 0) {
-        var querySelectExam0 = "SELECT title FROM exam_cat0";
+        var querySelectExam0 = "SELECT id, title FROM exam_cat0";
         db.get().query(querySelectExam0, function (err, rows) {
+           console
+
             if (err) return res.status(400).send(err);
             return res.status(200).send(JSON.stringify(rows));
         });
     } else if (req.query.examCat == 1) {
-        var querySelectExam1 = "SELECT title FROM exam_cat1 WHERE parent_id = ?";
+        var querySelectExam1 = "SELECT id, title FROM exam_cat1 WHERE parent_id = ?";
         db.get().query(querySelectExam1, req.query.examParentId, function (err, rows) {
             if (err) return res.status(400).send(err);
             return res.status(200).send(JSON.stringify(rows));
         });
     } else if ((req.query.examCat == 2)) {
-        var querySelectExam2 = "SELECT title FROM exam_cat2 WHERE parent_id = ?";
+        var querySelectExam2 = "SELECT id, title FROM exam_cat2 WHERE parent_id = ?";
         db.get().query(querySelectExam2, req.query.examParentId, function(err, rows) {
             if (err) return res.status(400).send(err);
             return res.status(200).send(JSON.stringify(rows));
@@ -37,7 +39,7 @@ router.get('/selectExam', function (req, res, next) {
 //과목 목록을 보여줌
 //REQ: examAddress
 router.get('/selectSubject', function(req, res, next) {
-    var querySelectSubject = "SELECT title FROM subjects WHERE exam_address = ?";
+    var querySelectSubject = "SELECT id, title FROM subjects WHERE exam_address = ?";
     db.get().query(querySelectSubject, req.query.examAddress, function (err, rows) {
         if (err) return res.status(400).send(err);
         return res.status(200).send(JSON.stringify(rows));
--- routes/statistics.js
+++ routes/statistics.js
@@ -3,7 +3,27 @@ var router = express.Router();
 var db = require('../config/db');
 var moment = require('moment');
 
-//통계-요약탭
+
+function loadDdayData(tartgetTime, userId) {
+    if(targetTime == undefined) {
+        var targetTime = moment().format('YYYY-MM-DD');
+    }
+    var querySelectHistories = "SELECT SUM(term) AS total_time, SUM(term) / (SELECT today_goal FROM user_goals WHERE reg_time <= ? + 1 LIMIT 1) AS goal_completion_rate, " +
+        + "SUM(term) / COUNT(term) AS concentration_time FROM histories " +
+        + "WHERE user_id = ? AND exam_address = (SELECT exam_address FROM user_settings d WHERE d.user_id = ?) AND end_point >= ?";
+
+    db.get().query(querySelectHistories, [req.query.targetTime, req.query.userId, req.query.targetTime], function (err, rows) {
+        if (err) return res.status(400).send(err);
+        return res.status(200).send(JSON.stringify(rows));
+    });
+}
+
+//REQ: targetTime(미지정시 오늘), userId
+//1일치 정보(총공부시간,목표달성률,연속집중력) 불러오기
+router.get('loadDay', function(req, res, next) {
+    var result1 = loadDayData(req.query.targetTime, req.query.userId);
+
+});
 
 router.get('/loadSummaryData', function(req, res, next) {
     var nowTime = moment().format('YYYY-MM-DD');
