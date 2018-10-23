627202c8b0b17f52028aaa7f933a78d6483fbc66
zwan2 <2zwan@naver.com>
2018-10-23 16:40:21 +0900

    Update routes/stopwatch.js
    stopwatch 만지작

--- routes/stopwatch.js
+++ routes/stopwatch.js
@@ -14,7 +14,7 @@ router.get('/', function (req, res, next) {
 router.get('/loadMain', isAuthenticated, function (req, res, next) {
     
     //[1] loadSettings
-    var examAddress, examTitle;
+    var examAddress, subjectIds, examTitle;
     var querySelectSettings = "SELECT exam_address, subject_ids FROM user_settings WHERE user_id = ?";
     var querySelectExamCat = "(SELECT title FROM exam_cat0 WHERE id = ?) UNION (SELECT title FROM exam_cat1 WHERE id = ?) UNION (SELECT title FROM exam_cat2 WHERE id = ?)"
     //user_settings의 exam_address와 subject_ids로 id->이름 불러옴
@@ -25,6 +25,7 @@ router.get('/loadMain', isAuthenticated, function (req, res, next) {
             return res.sendStatus(401);
         } else {
             examAddress = rows1[0].exam_address.split('_');
+            subjectIds = rows1[0].subject_ids;
         }
 
         db.get().query(querySelectExamCat, [examAddress[0], examAddress[1], examAddress[2]], function (err, rows2) {
@@ -43,16 +44,26 @@ router.get('/loadMain', isAuthenticated, function (req, res, next) {
 
                 var nowTime = moment().format('YYYY-MM-DD');
                 var querySelectGoals = "SELECT today_goal AS todayGoal, subject_goals AS subjectGoals FROM user_goals WHERE user_id = ? ORDER BY reg_time DESC LIMIT 1";
-                var querySelectHistory = "SELECT subject_id AS subjectId, SUM(term) AS subjectTotal FROM histories WHERE user_id = ? AND exam_address = (SELECT exam_address FROM user_settings d WHERE d.user_id = ?) AND subject_id IN(SELECT subject_ids FROM user_settings d WHERE d.user_id = ?)  AND end_point >= ? GROUP BY subject_id";
+                /*
+                var q = "SELECT subject_ids FROM user_settings WHERE user_id = ?";
+                db.get().query(q, [req.query.userId], function (err, rows4) {
+                    console.log(rows4);
+
+                });
+*/
+console.log(subjectIds);
+
+                var querySelectHistory = "SELECT subject_id AS subjectId, SUM(term) AS subjectTotal FROM histories WHERE user_id = ? AND exam_address = ? AND subject_id IN (?) AND end_point >= ? GROUP BY subject_id";
                 //[2] LoadHistory
                 db.get().query(querySelectGoals, [req.query.userId, req.query.userId], function (err, rows4) {
                     if (err) return res.status(400).send(err);
-                    db.get().query(querySelectHistory, [req.query.userId, req.query.userId, req.query.userId, nowTime], function (err, rows5) {
+                    db.get().query(querySelectHistory, [req.query.userId, rows1[0].exam_address, subjectIds, nowTime], function (err, rows5) {
                         if (err) return res.status(400).send(err);
                         var todayTotal = 0;
                         for (var i in rows5) {                      
                             todayTotal = todayTotal + rows5[i].subjectTotal;
                         }
+                        //return res.status(200).send(rows5);
 
                         
                         var loadSettingsResult = {
