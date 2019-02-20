4fda0647c342daa5166c72946d412baf836f0b18
zwan2 <2zwan@naver.com>
2019-02-20 16:51:12 +0900

    firebase schedule 12

--- app.js
+++ app.js
@@ -137,7 +137,7 @@ admin.initializeApp({
 });
 var db = admin.firestore();
 
-cron.schedule('* 0 6 * * *', function () {
+cron.schedule('* 0 0 * * *', function () {
   console.log('Firebase 삭제 시작');
 
   deleteCollection(db, 'study', 1000).then(function () {
